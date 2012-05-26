b1bfdfaef17e52739705e1008a15010e16a57a61
jk <jk@falcon.Home>
2012-05-27 00:31:59 +0200

    build new ratings file out of rankings

--- RatingsFile.py
+++ RatingsFile.py
@@ -17,90 +17,6 @@ from google.appengine.api import memcache
 
 import structures
 
-#class RatingsFile(webapp.RequestHandler):
-	#def get(self):
-		#parts = self.request.query_string.split("&")
-		#requests = {}
-		#if parts[0] != "":
-			#for pair in parts:
-				#ab = pair.split('=')
-				#requests[ab[0]] = ab[1]
-			
-		
-		#if "version" not in requests or requests["version"] != "1":
-			#return
-			
-		#game = requests.get("game",None)
-		
-		#if game is None:
-			#return
-		
-		#rumble = memcache.get(game)
-		#if rumble is None:
-			#rumble = structures.Rumble.get_by_key_name(game)
-			#if rumble is not None:
-				#memcache.set(game,rumble)
-		#if rumble is None:
-			#return ""
-			
-		##botnames = rumble.Participants.split("|")
-		##botHashes = [n + "|" + game for n in botnames]
-		##botdict = memcache.get_multi(botHashes)
-		##bots = [botdict.get(h,None) for h in botHashes]
-		##hashesMissing = []
-		##hmIndexes = []
-		##for i in range(len(bots)):
-			##if bots[i] is None:
-				##hashesMissing.append(botHashes[i])
-				##hmIndexes.append(i)
-		##dbbots = structures.BotEntry.get_by_key_name(hashesMissing)
-		
-		##for i in range(len(dbbots)):
-			##bots[hmIndexes[i]] = dbbots[i]
-			##memcache.set(hashesMissing[i],dbbots[i])
-		
-		#botNames = memcache.get(game + "|" + structures.participants)
-		#r = None
-		#if botNames is None or len(botNames) == 0 or (len(botNames) == 1 and botNames[0] == ""):
-			#botNames = None
-			#q = structures.BotEntry.all()
-			#q.filter("Rumble =",game)
-			#q.filter("Active =",True)
-			#r = q.run()
-			##structures.prefetch_refprops(r,structures.BotEntry.Name, structures.BotEntry.APS, structures.BotEntry.Battles, structures.BotEntry.LastUpload)
-		#else:
-			#botHashes = [b + "|" + game for b in botNames]
-			#rdict = memcache.get_multi(botHashes)
-			#r = [rdict.get(h,None) for h in botHashes]
-			#missingHashes = []
-			#missingIndexes = []
-			#for i in range(len(r)):
-				#if r[i] is None:
-					#missingHashes.append(botHashes[i])
-					#missingIndexes.append(i)
-			
-			#rmis = structures.BotEntry.get_by_key_name(missingHashes)
-			#for i in range(len(missingHashes)):
-				#r[missingIndexes[i]] = rmis[i]
-				
-			
-		#outstr = ""
-		#bots = {}
-		#botlist = []
-		#for bot in r:
-			#line = bot.Name + "=" + str(bot.APS) + "," + str(bot.Battles) + "," + str(bot.LastUpload) + "\n"
-			#outstr += line
-			#bots[bot.key().name()] = bot
-			#botlist.append(bot.Name)
-			##memcache.set(bot.key().name(),bot)
-		#memcache.set_multi(bots)
-
-		#if botNames is None:
-			#memcache.set(game + "|" + structures.participants,botlist)
-			#rumble.Participants = botlist
-			#db.put(rumble)
-			
-		#self.response.out.write(outstr)
 		
 class RatingsFile(webapp.RequestHandler):
 	def get(self):
@@ -135,28 +51,37 @@ class RatingsFile(webapp.RequestHandler):
 		r = [rdict.get(h,None) for h in botHashes]
 		missingHashes = []
 		missingIndexes = []
-		for i in range(len(r)):
+		for i in xrange(len(r)):
 			if r[i] is None:
 				missingHashes.append(botHashes[i])
 				missingIndexes.append(i)
-		
-		rmis = structures.BotEntry.get_by_key_name(missingHashes)
-		for i in range(len(missingHashes)):
-			r[missingIndexes[i]] = rmis[i]
+		if len(missingHashes) > 0:
+			rmis = structures.BotEntry.get_by_key_name(missingHashes)
+			lost = False
+			botsdict = {}
+			for i in xrange(len(missingHashes)):
+				if rmis[i] is not None:
+					r[missingIndexes[i]] = rmis[i]
+					botsdict[rmis[i].key().name()] = rmis[i]
+				else:
+					partSet = set(rumble.Participants)
+					partSet.discard(missingHashes[i])
+					rumble.Participants = list(partSet)
+					memcache.set(game,rumble)
+					rumble.put()
+					lost = True
+									
+			memcache.set_multi(botsdict)
+			if lost:
+				bots = filter(lambda b: b is not None, bots)
 		
 		outstr = ""
 		for bot in r:
-			line = bot.Name + "=" + str(bot.APS) + "," + str(bot.Battles) + "," + str(bot.LastUpload) + "\n"
+			name = bot.Name
+			name = name.replace(" ","_")
+			line = name + "=" + str(bot.APS) + "," + str(bot.Battles) + "," + str(bot.LastUpload) + "\n"
 			outstr += line
 
-		self.response.out.write(outstr)
-		
-		if len(rmis) > 0:
-			botsdict = {}
-			for bot in rmis:
-				botsdict[bot.key().name()] = bot
-			memcache.set_multi(botsdict)
-			
 		self.response.out.write(outstr)
 		
 
