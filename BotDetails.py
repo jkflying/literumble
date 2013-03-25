1aee44173728ec8f1d64fd6428871fea3a6b1d4a
jk <jk@FALCON.Home>
2013-03-25 17:55:01 +0200

    add API access

--- BotDetails.py
+++ BotDetails.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import cgi
+#import cgi
 import datetime
 import wsgiref.handlers
 import time
@@ -12,16 +12,18 @@ import string
 import zlib
 import cPickle as pickle
 
-from google.appengine.ext import db
-from google.appengine.api import users
+#from google.appengine.ext import db
+#from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.api import memcache
 from operator import attrgetter
 import structures
+from structures import global_dict
 
 class BotDetails(webapp.RequestHandler):
     def get(self):
-        global_dict = {}
+#        global_dict = {}
+        global global_dict
         starttime = time.time()
         query = self.request.query_string
         query = query.replace("%20"," ")
@@ -45,6 +47,7 @@ class BotDetails(webapp.RequestHandler):
         lim = int(requests.get("limit","10000000"))
         order = requests.get("order",None)
         timing = bool(requests.get("timing",False))
+        api = bool(requests.get("api",False))
         
         
         extraArgs = ""
@@ -69,11 +72,10 @@ class BotDetails(webapp.RequestHandler):
         
         cached = True
         keyhash = name + "|" + game
-        bot = memcache.get(keyhash)
+        bot = global_dict.get(keyhash,None)
         if bot is None:
-            bot = global_dict.get(keyhash,None)
-        else:
-            global_dict[keyhash] = bot
+            bot = memcache.get(keyhash)
+#            global_dict[keyhash] = bot
             
         if bot is None or bot.PairingsList is None:
             bot = structures.BotEntry.get_by_key_name(keyhash)
@@ -81,137 +83,199 @@ class BotDetails(webapp.RequestHandler):
             if bot is not None:
                 
                 memcache.set(keyhash,bot)
-                global_dict[keyhash] = bot
+#                global_dict[keyhash] = bot
                 cached = False
                 
         if bot is None:
             self.response.out.write( "ERROR. name/game combination does not exist: " + name + "/" + game)
         else:
             bots = None
-
-            try:
-                botsDicts = json.loads(zlib.decompress(bot.PairingsList))
-                bots = [structures.ScoreSet() for _ in botsDicts]
-                for s,d in zip(bots,botsDicts):
-                    s.__dict__.update(d)
-            except:
-                bots = pickle.loads(zlib.decompress(bot.PairingsList))
-
+            if lim > 0:
+                try:
+                    bots = pickle.loads(zlib.decompress(bot.PairingsList))
+                except:
+                    botsDicts = json.loads(zlib.decompress(bot.PairingsList))
+                    bots = [structures.ScoreSet() for _ in botsDicts]
+                    for s,d in zip(bots,botsDicts):
+                        s.__dict__.update(d)
+                for b in bots:
+                    lastUpload = None
+                    try:
+                        lastUpload = b.LastUpload
+                    except:
+                        b.LastUpload = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             
             retrievetime = time.time() - parsetime - starttime
             
-            for b in bots:
-                lastUpload = None
-                try:
-                    lastUpload = b.LastUpload
-                except:
-                    b.LastUpload = datetime.datetime.now()
-
-            bots = sorted(bots, key=attrgetter(order), reverse=reverseSort)
+            if(lim > 0):
+                bots = sorted(bots, key=attrgetter(order), reverse=reverseSort)            
             
-            sorttime = time.time() - retrievetime - parsetime - starttime
-            if order == "LastUpload":
-                order = "Latest Battle"
-            
-            out = []
-            
-            gameHref = "<a href=\"Rankings?game=" + game + extraArgs + "\">" + game + "</a>"
-            gameTitle = "Bot details of <b>" + name + "</b> in "+ gameHref + " vs. " + str(len(bots)) + " bots."
-            
-            out.append(structures.html_header % (game,gameTitle))
-            out.append("<table>\n")
-            out.append("<tr>\n<th>Name</th>\n<td>\n" + name + "</td></tr>")
-            out.append("<tr>\n<th>APS</th>\n<td>\n" + str(bot.APS) + "</td></tr>")
-            out.append("<tr>\n<th>PWIN</th>\n<td>\n" + str(50.0*float(bot.PL)/bot.Pairings + 50.0) + "</td></tr>")
-            out.append("<tr>\n<th>ANPP</th>\n<td>\n" + str(bot.ANPP) + "</td></tr>")
-            out.append("<tr>\n<th>Vote</th>\n<td>\n" + str(bot.VoteScore) + "</td></tr>")
-            out.append("<tr>\n<th>Survival</th>\n<td>\n" + str(bot.Survival) + "</td></tr>")
-            out.append("<tr>\n<th>Pairings</th>\n<td>\n" + str(bot.Pairings) + "</td></tr>")
-            out.append("<tr>\n<th>Battles</th>\n<td>\n" + str(bot.Battles) + "</td></tr>")
-            out.append("<tr>\n<th>Latest Battle</th>\n<td>\n" + str(bot.LastUpload) + " UTC</td></tr>")
-            out.append("<tr>\n<td colspan=\"2\">")
-            out.append("<form name=\"input\" action=\"BotCompare\" method=\"get\">")
-            out.append("<input type=\"hidden\" name=\"game\" value=\"" + game+ "\" />")
-            out.append("<input type=\"hidden\" name=\"bota\" value=\"" + name + "\" />")
-            out.append("<input type=\"text\" name=\"botb\" value=\"" + name + "\" />")
-            out.append("<input type=\"submit\" value=\"Compare\" formtarget=\"_blank\" /></form>")
-            out.append("</td></tr></table>\n<table>\n<tr>\n")
-
-            headings = ["  ",
-            "Name",
-            "",
-            "APS",
-            "NPP",
-            "Survival",
-            "KNNPBI",
-            "Battles",
-            "Latest Battle"]
-            
-            for heading in headings:
-                sortedBy = (order == heading)
-                headinglink = heading
-                if sortedBy and reverseSort:
-                    heading = "-" + heading
-                    headinglink = heading
-                elif not sortedBy:
-                    headinglink = "-" + headinglink
+            if api:
+                outs = ["{"]
+                outs.append("\n\"name\":\"")
+                outs.append(name)
+                outs.append("\",\n\"APS\":")
+                outs.append(str(bot.APS))
+                outs.append(",\n\"PWIN\":")
+                outs.append(str(50.0*float(bot.PL)/bot.Pairings + 50.0))
+                outs.append(",\n\"ANPP\":")
+                outs.append(str(bot.ANPP))
+                outs.append(",\n\"vote\":")
+                outs.append(str(bot.VoteScore))
+                outs.append(",\n\"survival\":")
+                outs.append(str(bot.Survival))
+                outs.append(",\n\"pairings\":")
+                outs.append(str(bot.Pairings))
+                outs.append(",\n\"battles\":")
+                outs.append(str(bot.Battles))
+                outs.append(",\n\"latest\":\"")
+                outs.append(str(bot.LastUpload))
+                outs.append("\"")
+                if lim > 0:
+                    outs.append(",\n\"pairingsList\":[\n")
+                    headings = [
+                    "\"name\"",
+                    "\"rank\"",
+                    "\"APS\"",
+                    "\"NPP\"",
+                    "\"survival\"",
+                    "\"KNNPBI\"",
+                    "\"battles\"",
+                    "\"latest\""]
+                    escapes = ["\"","","","","","","","\""]
+                    count = 0
+                    for b in bots:
+                        count += 1
+                        if count > lim:
+                            break
+                        
+                        cells = [b.Name,
+                                 count,
+                        round(100.0*b.APS)*0.01,
+                        round(100.0*b.NPP)*0.01,
+                        round(100.0*b.Survival)*0.01,
+                        round(100.0*b.KNNPBI)*0.01,
+                        b.Battles,b.LastUpload]
+                        
+                        outs.append("    {")
+                        for i in range(len(cells)):
+                            outs.append(headings[i])
+                            outs.append(":")
+                            outs.append(escapes[i])
+                            outs.append(str(cells[i]))
+                            outs.append(escapes[i])
+                            outs.append(",")
+                        outs[-1] = "},\n"
+                    outs[-1] = "}\n]"
+                outs.append("\n}")
+                self.response.out.write(''.join(outs))
+                
+            else:
+                                
+                sorttime = time.time() - retrievetime - parsetime - starttime
+                if order == "LastUpload":
+                    order = "Latest Battle"
+                
+                out = []
+                
+                gameHref = "<a href=\"Rankings?game=" + game + extraArgs + "\">" + game + "</a>"
+                gameTitle = "Bot details of <b>" + name + "</b> in "+ gameHref + " vs. " + str(len(bots)) + " bots."
+                
+                out.append(structures.html_header % (game,gameTitle))
+                out.append("<table>\n")
+                out.append("<tr>\n<th>Name</th>\n<td>\n" + name + "</td></tr>")
+                out.append("<tr>\n<th>APS</th>\n<td>\n" + str(bot.APS) + "</td></tr>")
+                out.append("<tr>\n<th>PWIN</th>\n<td>\n" + str(50.0*float(bot.PL)/bot.Pairings + 50.0) + "</td></tr>")
+                out.append("<tr>\n<th>ANPP</th>\n<td>\n" + str(bot.ANPP) + "</td></tr>")
+                out.append("<tr>\n<th>Vote</th>\n<td>\n" + str(bot.VoteScore) + "</td></tr>")
+                out.append("<tr>\n<th>Survival</th>\n<td>\n" + str(bot.Survival) + "</td></tr>")
+                out.append("<tr>\n<th>Pairings</th>\n<td>\n" + str(bot.Pairings) + "</td></tr>")
+                out.append("<tr>\n<th>Battles</th>\n<td>\n" + str(bot.Battles) + "</td></tr>")
+                out.append("<tr>\n<th>Latest Battle</th>\n<td>\n" + str(bot.LastUpload) + " UTC</td></tr>")
+                out.append("<tr>\n<td colspan=\"2\">")
+                out.append("<form name=\"input\" action=\"BotCompare\" method=\"get\">")
+                out.append("<input type=\"hidden\" name=\"game\" value=\"" + game+ "\" />")
+                out.append("<input type=\"hidden\" name=\"bota\" value=\"" + name + "\" />")
+                out.append("<input type=\"text\" name=\"botb\" value=\"" + name + "\" />")
+                out.append("<input type=\"submit\" value=\"Compare\" formtarget=\"_blank\" /></form>")
+                out.append("</td></tr></table>\n<table>\n<tr>\n")
+                
+                if lim > 0:
+                    headings = ["  ",
+                    "Name",
+                    "",
+                    "APS",
+                    "NPP",
+                    "Survival",
+                    "KNNPBI",
+                    "Battles",
+                    "Latest Battle"]
                     
-                orderHref = "<a href=\"BotDetails?game="+game+"&amp;name="+name.replace(" ","%20")+"&amp;order="+ headinglink.replace(" ","%20") + extraArgs + "\">"+heading+"</a>"
-                if sortedBy:
-                    out.append(  "\n<th class=\"sortedby\">" + orderHref + "</th>")
-                else:
-                    out.append(  "\n<th>" + orderHref + "</th>")
-            out.append(  "\n</tr>")
-            rank = 1
-            highlightKey = [False,False,False,True,True,True,True,False,False]
-            mins = [0,0,0,40,40,40,-0.1,0,0]
-            maxs = [0,0,0,60,70,60,0.1,0,0]
-            for bot in bots:
-                if rank > lim:
-                    break
-
-                botName=bot.Name
-                botNameHref = "<a href=\"BotDetails?game="+game+"&amp;name=" + botName.replace(" ","%20")+extraArgs+"\">"+botName+" </a>"
-                compareHref = "<a href=\"BotCompare?game="+game+"&amp;bota=" + name.replace(" ","%20") + "&amp;botb=" + botName.replace(" ","%20") + extraArgs + "\" target=\"_blank\">compare</a>"
-                cells = [str(rank),
-                botNameHref,
-                compareHref,
-                round(100.0*bot.APS)*0.01,
-                round(100.0*bot.NPP)*0.01,
-                round(100.0*bot.Survival)*0.01,
-                round(100.0*bot.KNNPBI)*0.01,
-                bot.Battles,
-                bot.LastUpload]
-                
-                out.append("\n<tr>")
-                for i,cell in enumerate(cells):
-                    if highlightKey[i]:
-                        if cell < mins[i]:
-                            out.append(  "\n<td class=\"red\">" + str(cell) + "</td>")
-                        elif cell > maxs[i]:
-                            out.append(  "\n<td class=\"green\">" + str(cell) + "</td>")
+                    for heading in headings:
+                        sortedBy = (order == heading)
+                        headinglink = heading
+                        if sortedBy and reverseSort:
+                            heading = "-" + heading
+                            headinglink = heading
+                        elif not sortedBy:
+                            headinglink = "-" + headinglink
+                            
+                        orderHref = "<a href=\"BotDetails?game="+game+"&amp;name="+name.replace(" ","%20")+"&amp;order="+ headinglink.replace(" ","%20") + extraArgs + "\">"+heading+"</a>"
+                        if sortedBy:
+                            out.append(  "\n<th class=\"sortedby\">" + orderHref + "</th>")
                         else:
-                            out.append(  "\n<td>" + str(cell) + "</td>")
-                    else:
-                        out.append(  "\n<td>" + str(cell) + "</td>")
-                out.append( "\n</tr>")
+                            out.append(  "\n<th>" + orderHref + "</th>")
+                    out.append(  "\n</tr>")
+                    rank = 1
+                    highlightKey = [False,False,False,True,True,True,True,False,False]
+                    mins = [0,0,0,40,40,40,-0.1,0,0]
+                    maxs = [0,0,0,60,70,60,0.1,0,0]
+                    for bot in bots:
+                        if rank > lim:
+                            break
+        
+                        botName=bot.Name
+                        botNameHref = "<a href=\"BotDetails?game="+game+"&amp;name=" + botName.replace(" ","%20")+extraArgs+"\">"+botName+" </a>"
+                        compareHref = "<a href=\"BotCompare?game="+game+"&amp;bota=" + name.replace(" ","%20") + "&amp;botb=" + botName.replace(" ","%20") + extraArgs + "\" target=\"_blank\">compare</a>"
+                        cells = [str(rank),
+                        botNameHref,
+                        compareHref,
+                        round(100.0*bot.APS)*0.01,
+                        round(100.0*bot.NPP)*0.01,
+                        round(100.0*bot.Survival)*0.01,
+                        round(100.0*bot.KNNPBI)*0.01,
+                        bot.Battles,
+                        bot.LastUpload]
+                        
+                        out.append("\n<tr>")
+                        for i,cell in enumerate(cells):
+                            if highlightKey[i]:
+                                if cell < mins[i]:
+                                    out.append(  "\n<td class=\"red\">" + str(cell) + "</td>")
+                                elif cell > maxs[i]:
+                                    out.append(  "\n<td class=\"green\">" + str(cell) + "</td>")
+                                else:
+                                    out.append(  "\n<td>" + str(cell) + "</td>")
+                            else:
+                                out.append(  "\n<td>" + str(cell) + "</td>")
+                        out.append( "\n</tr>")
+                        
+                        rank += 1
+                        
+                    out.append(  "</table>")
+                htmltime = time.time() - sorttime - retrievetime - parsetime - starttime 
+                elapsed = time.time() - starttime
+                if timing:
+                    out.append(  "<br />\n Page served in " + str(int(round(elapsed*1000))) + "ms. Bot cached: " + str(cached))
+                    out.append("\n<br /> parsing: " + str(int(round(parsetime*1000))) )
+                    out.append("\n<br /> retrieve: " + str(int(round(retrievetime*1000))) )
+                    out.append("\n<br /> sort: " + str(int(round(sorttime*1000))) )
+                    out.append("\n<br /> html generation: " + str(int(round(htmltime*1000))) )
+                out.append(  "</body></html>")
                 
-                rank += 1
-                
-            out.append(  "</table>")
-            htmltime = time.time() - sorttime - retrievetime - parsetime - starttime 
-            elapsed = time.time() - starttime
-            if timing:
-                out.append(  "<br />\n Page served in " + str(int(round(elapsed*1000))) + "ms. Bot cached: " + str(cached))
-                out.append("\n<br /> parsing: " + str(int(round(parsetime*1000))) )
-                out.append("\n<br /> retrieve: " + str(int(round(retrievetime*1000))) )
-                out.append("\n<br /> sort: " + str(int(round(sorttime*1000))) )
-                out.append("\n<br /> html generation: " + str(int(round(htmltime*1000))) )
-            out.append(  "</body></html>")
-            
-            outstr = string.join(out,"")
-                
-            self.response.out.write(outstr)
+                outstr = string.join(out,"")
+                    
+                self.response.out.write(outstr)
 
 
 application = webapp.WSGIApplication([
