2dd9f93352ea6433bf32c1be62b9b530b18a126a
julian <julian@nomanini.com>
2014-04-06 21:19:39 +0200

    Add flag for Belarus

--- structures.py
+++ structures.py
@@ -153,7 +153,7 @@ class Rumble(db.Model):
 default_flag_map = "FLAGMAP"
 allowed_flags = set(['ABW', 'AFG', 'AGO', 'AIA', 'ALA', 'ALB', 'AND', 'ARE', 
                      'ARG', 'ARM', 'ASM', 'ATA', 'ATF', 'ATG', 'AUS', 'AUT', 
-                     'AZE', 'BDI', 'ANK', 'BRA', 'CAN', 'CHE', 'CHN', 'CZE', 'DEU', 
+                     'AZE', 'ANK', 'BDI', 'BLR', 'BRA', 'CAN', 'CHE', 'CHN', 'CZE', 'DEU', 
                      'ESP', 'FIN', 'FRA', 'GBR', 'HUN', 'IRL', 'ITA', 'JPN', 
                      'KOR', 'LTU', 'MAN', 'NLD', 'NOR', 'NZL', 'POL', 'PRT', 'RUS', 'SGP', 
                      'SRB', 'SWE', 'SVK','THA', 'USA', 'VEN', 'WIKI', 'ZAF'])
