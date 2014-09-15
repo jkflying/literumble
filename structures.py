0fe977075115b3af69c2b1b442139731f2749410
jkflying <jkflying@gmail.com>
2014-09-15 21:40:19 +0200

    Only allow 1.9.2.3 clients

--- structures.py
+++ structures.py
@@ -20,7 +20,7 @@ from google.appengine.ext import db
 total = "TOTAL"
 participants = "PARTICIPANTS"
 sync = "SYNCHRONIZE"
-allowed_clients = ["1.9.2.2"]
+allowed_clients = ["1.9.2.3"]
 allowed_versions = ["1"]
 
 global_dict = {}
